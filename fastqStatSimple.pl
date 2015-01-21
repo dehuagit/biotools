b7f7507516674c66c3ba6fa13b3bfab0487d93e9
Andy Chung <andy_chung@ibms.sinica.edu.tw>
2015-01-21 15:48:07 +0800

    fix

--- fastqStatSimple.pl
+++ fastqStatSimple.pl
@@ -4,9 +4,6 @@ use warnings;
 use strict;
 
 my $fastaFile = shift;
-
-open (FASTA, "<$fastaFile");
-
 my $c_sum = 0;
 my $g_sum = 0;
 my $t_sum = 0;
@@ -14,25 +11,33 @@ my $a_sum = 0;
 my $all_length = 0;
 my $line_index=0;
 
-while (my $line = <FASTA>) {
-	chomp $line;
 
-	my $seq = $line;
-	if ($line_index % 4 == 1) {
-	#	print "seq: $seq"."\n";
-		$all_length = $all_length + length($seq);
-		my $c = ($seq=~tr/C//);
-		my $t = ($seq=~tr/T//);
-		my $g = ($seq=~tr/G//);
-		my $a = ($seq=~tr/A//);
-		$c_sum = $c_sum + $c;
-		$g_sum = $g_sum + $g;
-		$t_sum = $t_sum + $t;
-		$a_sum = $a_sum + $a;
+my $in;
+if (defined $fastaFile) {
+	open ($in, "<$fastaFile");
+}
+else {
+	$in = *STDIN;
+}
+
+	while (my $line = <$in>) {
+		chomp $line;
+		my $seq = $line;
+		if ($line_index % 4 == 1) {
+		#	print "seq: $seq"."\n";
+			$all_length = $all_length + length($seq);
+			my $c = ($seq=~tr/C//);
+			my $t = ($seq=~tr/T//);
+			my $g = ($seq=~tr/G//);
+			my $a = ($seq=~tr/A//);
+			$c_sum = $c_sum + $c;
+			$g_sum = $g_sum + $g;
+			$t_sum = $t_sum + $t;
+			$a_sum = $a_sum + $a;
+		}
+		$line_index = $line_index + 1;
 	}
 
-	$line_index = $line_index + 1;
-}
 
 my $n_sum = $all_length - $c_sum - $g_sum - $t_sum - $a_sum;
 
